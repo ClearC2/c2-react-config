3868e9a418c8a210c6c58f21babb8519743a8e1e
David Adams <adams.david.10@gmail.com>
2018-10-17 09:39:10 -0500

    fix(exports): update index exports

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const webpackConfig = require('./webpack/webpack.config')
+const webpackUtils = require('./webpack/utils')
 const babelConfig = require('./babel.config')
 
 module.exports = {
   webpackConfig,
+  webpackUtils,
   babelConfig
 }
