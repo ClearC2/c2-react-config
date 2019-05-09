b0342f9066144829db96988b8132efcba03b1f28
David Adams <adams.david.10@gmail.com>
2019-05-09 14:43:19 -0500

    perf(hot-loader): add hot loader alias for better hot reloading
    BREAKING CHANGE: requires project to `yarn add @hot-loader/react-dom`. requires `hot` to be imported from `import {hot} from 'react-hot-loader/root'`.
    requires change from `export default hot(module)(App)` to `export default hot(App)`

---
+++ example/src/App/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Counter () {
+  const [count, setCount] = React.useState(0)
+  return (
+    <button onClick={() => setCount(count + 1)}>
+      {count}
+    </button>
+  )
+}
+
+export default Counter
