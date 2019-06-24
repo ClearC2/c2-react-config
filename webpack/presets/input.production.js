d0ada9e0f6925132ae0c0a76f848bb2a420e2ca0
David Adams <adams.david.10@gmail.com>
2019-06-24 13:07:58 -0500

    fix: upgrade deps

--- webpack/presets/input.production.js
+++ webpack/presets/input.production.js
@@ -2,7 +2,8 @@ const path = require('path')
 
 module.exports = (env) => ({
   entry: [
-    '@babel/polyfill',
+    'core-js/stable',
+    'regenerator-runtime/runtime',
     'url-search-params-polyfill',
     path.join(env.projectDir, 'src', 'index.js')
   ]
