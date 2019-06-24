499cfd437a44dc5b0d0043ac7e11826e91c80ff8
David Adams <adams.david.10@gmail.com>
2019-06-24 13:59:09 -0500

    fix: core-js

--- webpack/presets/input.production.js
+++ webpack/presets/input.production.js
@@ -2,9 +2,9 @@ const path = require('path')
 
 module.exports = (env) => ({
   entry: [
-    'core-js/stable',
-    'regenerator-runtime/runtime',
-    'url-search-params-polyfill',
+    require.resolve('core-js/stable'),
+    require.resolve('regenerator-runtime/runtime'),
+    require.resolve('url-search-params-polyfill'),
     path.join(env.projectDir, 'src', 'index.js')
   ]
 })
