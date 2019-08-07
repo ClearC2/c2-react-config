2c59ee49af10c98ecbd3136634874f1a2acf86c1
David Adams <adams.david.10@gmail.com>
2019-08-07 11:02:35 -0500

    fix: add prettier config object to package index

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const webpackConfig = require('./webpack/webpack.config')
 const webpackUtils = require('./webpack/utils')
 const babelConfig = require('./babel.config')
+const prettierConfig = require('./prettier.config')
 
 module.exports = {
   webpackConfig,
   webpackUtils,
-  babelConfig
+  babelConfig,
+  prettierConfig
 }
