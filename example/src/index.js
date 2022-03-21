5e38bd3e2432cb7bab3ae33729f51e4be9c4fd06
dadamssg <adams.david.10@gmail.com>
2022-03-21 11:25:33 -0500

    fix(sass): remove sass
    BREAKING CHANGE: remove the inclusion of sass support. require app to set that up

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {render} from 'react-dom'
 import 'bootstrap'
-import './scss/style.scss'
+// import './scss/style.scss'
+import 'bootstrap/dist/css/bootstrap.css'
 import App from './App'
 
 render(
